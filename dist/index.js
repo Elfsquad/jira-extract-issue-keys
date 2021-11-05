a75ded6560eea3aeaee3aaa3525d98dfc113acf6
Stan van Rooy <stan@rooy.dev>
2021-11-05 10:31:19 +0100

    update build

--- dist/index.js
+++ dist/index.js
@@ -8445,24 +8445,31 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2810);
 const github = __nccwpck_require__(4176);
 
-let octokit = undefined;
+const context = github.context;
 
 const getHeadReleaseTag = async () => {
   const response = await octokit.request("GET /repos/{owner}/{repo}/releases/latest", {
-    owner: github.context.owner,
-    repo: github.context.repo
+    owner: context.owner,
+    repo: context.repo
   });
+  console.log('Head release tag response: ', response);
   return response.tag_name;
 }
 
 const getDefaultBaseReleaseTag = async () => {
-
+  const response = await octokit.request("GET /GET /repos/{owner}/{repo}/releases/", {
+    owner: context.owner,
+    repo: context.repo
+  });
+  console.log('Base release tag response: ', response);
+  return response[0].tag_name;
 }
 
 (async function() {
   try {
     const token = core.getInput('token');
     octokit = github.getOctokit(token);
+    console.log("octokit: ", JSON.stringify(octokit));
     console.log("Initiated octokit");
 
     const headReleaseTag = await getHeadReleaseTag()
